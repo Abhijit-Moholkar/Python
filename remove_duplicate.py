eb4b351a2a68cabfbab38af8d24a4a3ee64c5b4c
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-11 02:19:29 -0700

    Update remove_duplicate.py

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,3 +1,4 @@
+# To remove duplicates of list using remove method
 l=[2,4,5,3,6,8,2,4,3,1,5,6,6,6,6]
 for i in l:
     c= l.count(i)
@@ -9,4 +10,27 @@ print(l)
 '''
 Output
 [3, 8, 2, 4, 3, 1, 5, 6]
+'''
+
+# To remove duplicates of list using another list
+
+l=[2,4,5,3,6,8,2,4,3,1,5,6,6,6,6]
+l1=[]
+for i in l:
+    if i not in l1:
+        l1.append(i)
+print(l1)
+'''
+Output
+[2, 4, 5, 3, 6, 8, 1]
+'''
+
+# To remove duplicates of list using set property
+
+l=[2,4,5,3,6,8,2,4,3,1,5,6,6,6,6]
+print(list(set(l)))
+
+'''
+Output
+[2, 4, 5, 3, 6, 8, 1]
 '''
