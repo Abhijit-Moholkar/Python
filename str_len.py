931c94471e8e5c24ca9f3a974477469815453dad
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-11 05:15:43 -0700

    Create str_len.py
    Ways to count length of string

---
+++ str_len.py
@@ -0,0 +1,22 @@
+# To count length of string
+s='python is intresting language'
+n=0
+for i in s:
+    n+=1
+print(n)
+
+# Output
+#29
+##########################################################
+# To count length of string
+s='python is intresting language'
+print(len(s))
+# Output
+#29
+######################################
+
+# To count length of string
+s='python is intresting language'
+print(s.rindex(s[-1])+1)
+# Output
+#29
