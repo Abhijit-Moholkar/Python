4590d2e115eba8da51356dd3a41355b049a8e78c
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-13 15:33:52 +0530

    Update ternary.py
    use of ternary operator to predict result

--- ternary.py
+++ ternary.py
@@ -3,10 +3,13 @@ b = 5
 # if [a<b] is true it return 1, so element with 1 index will print
 # else if [a<b] is false it return 0, so element with 0 index will print
 print( (b, a) [a < b] )  
+#############################################
 
 # Use Dictionary for selecting an item
 # if [a < b] is true then value of True key will print
 # elif [a<b] is false then value of False key will print
 print({True: a, False: b} [a < b])
 
- 
+###############################################
+
+print((lambda: b, lambda: a)[a < b]())
