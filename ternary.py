2cec4b3f9e04b257567b958a48c79f5923583433
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-13 15:31:43 +0530

    Create ternary.py
    Code to print result using ternary operator

---
+++ ternary.py
@@ -0,0 +1,12 @@
+a = 10 
+b = 5
+# if [a<b] is true it return 1, so element with 1 index will print
+# else if [a<b] is false it return 0, so element with 0 index will print
+print( (b, a) [a < b] )  
+
+# Use Dictionary for selecting an item
+# if [a < b] is true then value of True key will print
+# elif [a<b] is false then value of False key will print
+print({True: a, False: b} [a < b])
+
+ 
