fde3ac3e92fb100ed8e24de5198ca9ede00e4ce4
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-15 05:16:36 -0700

    Update symmetric_string.py
    A code to test string for symmetric. A string is symmetric if first half part of string exactly matches to second half

--- symmetric_string.py
+++ symmetric_string.py
@@ -4,3 +4,23 @@ if s[:l] == s[l:]:
     print("string is Symmetric")
 else:
     print("String is not Symmetric")
+    
+#####################################################################
+s = input("Enter your string: ")
+l = int(len(s)/2)
+for i in range(l):
+    if s[i] == s[l+i]:
+        pass
+    else:
+        print("String is not Symmetric")
+        break
+else:
+    print("string is Symmetric")
+####################################################################
+s='sanjsan'
+p=int(len(s)/2)
+#print(p)
+if s[0:p]==s[p:]:
+    print('symmetric')
+else:
+    print('unsymmetric')
