499f4a94947d23e726faaefd5725918f79061ef3
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-15 05:30:21 -0700

    Create os_module.py
    Code of OS module

---
+++ os_module.py
@@ -0,0 +1,22 @@
+import os
+
+'''print(dir,os)
+print(os.getcwd())
+print(os.environ.get('path'))
+os.chdir("c://")
+print(os.getcwd())
+print(os.listdir('c://'))
+os.mkdir('C:/Users/Moholkar/Desktop/python/ycode')'''
+#os.makedirs('C:/Users/Moholkar/Desktop/python/dcode/ccode')
+#os.mkdir('C:/Users/Moholkar/Desktop/python/2.txt')
+#os.rename('C:/Users/Moholkar/Desktop/python/2.txt','C:/Users/Moholkar/Desktop/python/5.txt')
+#os.chdir("C://Users/Moholkar/Desktop/python/")
+#print(os.listdir('c://Users/Moholkar/Desktop/python'))
+#os.rmdir("C:/Users/Moholkar/Desktop/python/2.txt")
+print(os.listdir('c://Users/Moholkar/Desktop/python'))
+#os.removedirs("C:/Users/Moholkar/Desktop/python/5.txt/a")
+print(os.listdir('c://Users/Moholkar/Desktop/python/5.txt'))
+print(os.path.join("C:/Users/Moholkar/Desktop/python/5.txt/b","C:/Users/Moholkar/Desktop/python/5.txt/c"))
+print(os.path.exists("C:/Users/Moholkar/Desktop/python/5.txt/b"))
+print(os.path.isfile('e.txt'))
+print(os.path.isdir("C:/Users/Moholkar/Desktop/python/5.txt/c"))
