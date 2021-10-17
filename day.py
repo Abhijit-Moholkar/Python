885f00ee5abf6e941450e34a49700eb2c926a33d
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-17 02:32:44 -0700

    Update day.py
    A code to get day in number and word
    
    output
    
    6 
    Sunday

--- day.py
+++ day.py
@@ -1,3 +1,5 @@
+# day extraction using datetime module
+
 from datetime import datetime
 
 given_date = datetime(2020, 7, 26)
@@ -5,3 +7,13 @@ given_date = datetime(2020, 7, 26)
 print(given_date.today().weekday())
 
 print(given_date.strftime('%A'))
+
+###########################################################
+# Day extraction using calender module
+
+import calendar
+from datetime import datetime
+
+given_date = datetime(2020, 7, 26)
+weekday = calendar.day_name[given_date.weekday()]
+print(weekday)
