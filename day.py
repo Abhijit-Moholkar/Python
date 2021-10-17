da0a02de1ce9c77ec0980100ad7b9c3adc7b0aa1
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-17 02:01:51 -0700

    Update day.py
    A code to get day from date
    
    output
    6
    Sunday

--- day.py
+++ day.py
@@ -5,3 +5,15 @@ given_date = datetime(2020, 7, 26)
 print(given_date.today().weekday())
 
 print(given_date.strftime('%A'))
+
+
+#################################################
+
+# Approach using calculator
+
+import calendar
+from datetime import datetime
+
+given_date = datetime(2020, 7, 26)
+weekday = calendar.day_name[given_date.weekday()]
+print(weekday)
