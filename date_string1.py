7a9303e1e04fea285dfccee475e4f947f376046c
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-17 02:35:28 -0700

    Create date_string1.py
    A code to get string format of given date
    
    output
    2020-00-02/25/20 00:00:00

---
+++ date_string1.py
@@ -0,0 +1,8 @@
+# To convert given date in to string
+
+from datetime import datetime, timedelta
+
+given_date = datetime(2020, 2, 25)
+
+string_data = given_date.strftime('%Y-%M-%D %H:%M:%S')
+print(string_data)
