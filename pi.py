4c62a3ac64a65ced7c2c2944b45f2fe8c76b6202
Abhijit-Moholkar <89727322+Abhijit-Moholkar@users.noreply.github.com>
2021-10-19 03:21:23 -0700

    Update pi.py
    Here, we want to get specified number of digits of pi
    
    output
    Enter your number: 1
    3.1

--- pi.py
+++ pi.py
@@ -3,3 +3,13 @@
 from math import pi
 n = int(input("Enter your number: "))
 print(f'%.{n}f'%pi)
+
+
+##############################################################
+
+# To get math.pi number up to specified number of decimal points
+
+from math import pi
+i = int(input("Enter your number: "))
+
+str(math.pi).split('.')[0]+'.'+str(math.pi).split('.')[1][:i]
